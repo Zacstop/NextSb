81a39dba075ab4f7b29b3a237c484c36216bff18
kang joonhyeong <zaca3392@gmail.com>
2023-11-19 13:29:37 +0900

    yarn settings

--- src/components/atoms/Footer/Footer.stories.ts
+++ src/components/atoms/Footer/Footer.stories.ts
@@ -9,7 +9,7 @@ export default meta;
 
 export const Default: StoryObj<typeof meta> = {
   args: {
-    title: 'XForm',
-    des: 'XForm, a patented technology by kubeCRO, revolutionizes clinical trials by allowing various data formats without CRF coding, simplifying the process. It enables anyone to transform CRF into ECRF, cutting preparation time by over 70%. The technology supports text, images, and videos in a user-friendly form, making it both intuitive and efficient.',
+    title: 'KubeCRO',
+    des: '빠르고 편리한 임상플랫폼',
   },
 };
